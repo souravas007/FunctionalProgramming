6bf30c513a0ff83d164ae9ce3e4e73ee54b160c1
sourav <sourav.s@pelatro.com>
2021-05-23 19:35:45 +0530

    Print odd & even numbers.

--- src/com/code/Main.java
+++ src/com/code/Main.java
@@ -6,21 +6,31 @@ public class Main {
 
 	public static void main( String[] args ) {
 		Main main = new Main();
-		main.printNumbersInListTraditional();
-		main.printNumberInListFunctionalProgramming();
+		//		main.printNumbersInListTraditional();
+		//		main.printNumberInListFunctionalProgramming();
+		main.printEvenNumbers();
+		main.printOddNumbers();
 	}
 
+	List<Integer> numbers = List.of( 12, 9, 13, 4, 15 );
+
 	private void printNumbersInListTraditional() {
-		List<Integer> numbers = List.of( 12, 9, 13, 4, 15 );
 		for ( Integer number : numbers ) {
 			System.out.println( number );
 		}
 	}
 
 	private void printNumberInListFunctionalProgramming() {
-		List<Integer> numbers = List.of( 12, 9, 13, 4, 15 );
 		numbers.stream().forEach( System.out::println );
 		numbers.stream().forEach( number -> System.out.println( number ) );
+	}
+
+	private void printEvenNumbers() {
+		numbers.stream().filter( x -> x % 2 == 0 ).forEach( System.out::println );
+	}
+
+	private void printOddNumbers() {
+		numbers.stream().filter( x -> x % 2 != 0 ).forEach( System.out::println );
 	}
 
 }
