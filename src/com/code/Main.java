e9196a3a3dfae6a8fde383cc3b083bcaca76346d
sourav <sourav.s@pelatro.com>
2021-05-23 19:27:05 +0530

    Print numbers with stream.

--- src/com/code/Main.java
+++ src/com/code/Main.java
@@ -1,8 +1,26 @@
 package com.code;
 
+import java.util.List;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
-    }
+	public static void main( String[] args ) {
+		Main main = new Main();
+		main.printNumbersInListTraditional();
+		main.printNumberInListFunctionalProgramming();
+	}
+
+	private void printNumbersInListTraditional() {
+		List<Integer> numbers = List.of( 12, 9, 13, 4, 15 );
+		for ( Integer number : numbers ) {
+			System.out.println( number );
+		}
+	}
+
+	private void printNumberInListFunctionalProgramming() {
+		List<Integer> numbers = List.of( 12, 9, 13, 4, 15 );
+		numbers.stream().forEach( System.out::println );
+		numbers.stream().forEach( number -> System.out.println( number ) );
+	}
+
 }
