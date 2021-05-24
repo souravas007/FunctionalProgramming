74ce6f71abeead18ff87754dfcdbff87fe8a04dd
sourav <sourav.s@pelatro.com>
2021-05-24 09:56:49 +0530

    Started Functional Interfaces & Lambda's.

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # FunctionalProgramming
 1. Basics
 2. Streams
+3. Functional Interfaces & Lambda's
---
+++ src/com/code/FunctionalInterfacesAndLambdas.java
@@ -0,0 +1,10 @@
+package com.code;
+
+public class FunctionalInterfacesAndLambdas {
+
+	public static void main( String[] args ) {
+		FunctionalInterfacesAndLambdas functionalInterfacesAndLambdas =
+				new FunctionalInterfacesAndLambdas();
+
+	}
+}
