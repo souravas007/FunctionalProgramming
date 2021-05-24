5effa2904d4d0fe112587dcaa4f15119a249de69
sourav <sourav.s@pelatro.com>
2021-05-24 22:19:51 +0530

    Sort.

--- src/com/code/CustomClasses.java
+++ src/com/code/CustomClasses.java
@@ -1,12 +1,15 @@
 package com.code;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
-public class CustomClasses {
+@SuppressWarnings( "SimplifyStreamApiCallChains" ) public class CustomClasses {
 
 	public static void main( String[] args ) {
 		CustomClasses customClass = new CustomClasses();
-		customClass.match();
+		//		customClass.match();
+		customClass.sort();
 
 	}
 
@@ -14,7 +17,7 @@ public class CustomClasses {
 			new Course( "Spring", "Framework", 98, 1000 ),
 			new Course( "Java", "Language", 88, 300 ),
 			new Course( "Python", "Language", 55, 100 ),
-			new Course( "React", "Framework", 22, 150 )
+			new Course( "React", "Framework", 88, 150 )
 	);
 
 	private void match() {
@@ -29,5 +32,24 @@ public class CustomClasses {
 		System.out.println(
 				courses.stream().anyMatch( course -> course.getReviewScore() > 95 ) );
 
+	}
+
+	private void sort() {
+		Comparator<Course> comparatorNoOfStudents =
+				Comparator.comparing( Course::getNoOfStudents );
+		courses.stream().sorted( comparatorNoOfStudents ).collect( Collectors.toList() )
+				.stream()
+				.forEach( System.out::println );
+		Comparator<Course> comparatorNoOfStudentsReversed =
+				Comparator.comparing( Course::getNoOfStudents ).reversed();
+		courses.stream().sorted( comparatorNoOfStudentsReversed )
+				.collect( Collectors.toList() ).stream()
+				.forEach( System.out::println );
+		Comparator<Course> comparatorNoOfStudentsAndReviewScoreReversed =
+				Comparator.comparing( Course::getNoOfStudents )
+						.thenComparing( Course::getReviewScore ).reversed();
+		courses.stream().sorted( comparatorNoOfStudentsAndReviewScoreReversed )
+				.collect( Collectors.toList() ).stream()
+				.forEach( System.out::println );
 	}
 }
