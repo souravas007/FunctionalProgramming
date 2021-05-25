c8a71501a02923787588cc344926ad69afe26c63
sourav <sourav.s@pelatro.com>
2021-05-25 11:18:25 +0530

    Stream from array.

--- src/com/code/FunctionalProgramming.java
+++ src/com/code/FunctionalProgramming.java
@@ -1,8 +1,19 @@
 package com.code;
 
+import java.util.Arrays;
+import java.util.stream.Stream;
+
 public class FunctionalProgramming {
 
 	public static void main( String[] args ) {
 		FunctionalProgramming functionalProgramming = new FunctionalProgramming();
+		functionalProgramming.streamFromArray();
+	}
+
+	private void streamFromArray() {
+		System.out.println( Stream.of( 12, 9, 13, 4 ).count() );
+		int[] numberArray = { 12, 9, 13, 4 };
+		System.out.println( Arrays.stream( numberArray ).sum() );
+
 	}
 }
